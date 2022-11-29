32220ddeec0e6b7b86afcadc23706d6f0c9ac10e
Plamenpowa <pkostadinov72@gmail.com>
2022-11-29 16:38:38 +0200

    adding articles per body click

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,15 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const body = document.body;
+  body.addEventListener("click", () => {
+    for (let i = 0; i < 5; i++) {
+      let article = document.createElement("article");
+      article.className = "message";
+      article.textContent = "sample";
+      body.append(article);
+    }
+  });
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
